3b3075b7dfb0e8745da8cc6644967619640a7ad9
root <km@rtin.mobi>
2022-12-05 10:29:46 +0000

    update task7 for view

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -3,7 +3,8 @@
   View of State object
 """
 from api.v1.views import app_views
-from models import storage, state
+from models import storage
+from models.state import State
 from flask import jsonify, abort, request
 
 
