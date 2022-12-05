1f6a9a432517bb9c96d0c32ee201648330550d03
root <km@rtin.mobi>
2022-12-05 10:20:21 +0000

    task7

---
+++ api/v1/views/states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+  View of State object
+"""
+from api.v1.views import app_views
+from models import storage, state
+from flask import jsonify, abort, request
+
+
+@app_views.route('/states', methods=["GET"])
+def states_ret():
+"""
+  Returns json State objects
+"""
+    state_list = []
+    all_objs = storage.all("State")
+    for obj in all_objs.values():
+        state_list.append(obj.to_dict())
+    return jsonify(state_list)
