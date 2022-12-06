4cb3fc3f9c23c2f1a2609722346a8517069ef694
Gotmished <5002@holbertonstudents.com>
2022-12-06 07:53:31 +0000

    Another attempt to fix documentation checker failure

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,14 +2,11 @@
 """The Flask app module"""
 from api.v1.views import app_views
 from flask import Flask, jsonify
-from flask_cors import CORS
 from models import storage
 from os import getenv
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
-app.config.update(JSONIFY_PRETTYPRINT_REGULAR=True)
 
 
 @app.teardown_appcontext
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""__init__ file"""
-
+"""Contains blueprints for api url constructor"""
 
 from flask import Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix="/api/v1")
-if app_views:
+
 from api.v1.views.index import *
 from api.v1.views.states import *
