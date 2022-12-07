8bf900f40fe0921d0002723ed3b6426221d0b0f3
Gotmished <5002@holbertonstudents.com>
2022-12-07 02:03:47 +0000

    Version two, but still unable to connect to MySQL to test

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -9,52 +9,50 @@ from models.city import City
 from models.state import State
 
 
-@app_views.route('/cities', methods=["GET"])
-def cities_serialise():
-    """Serialises City objects"""
-    city_list = []
-    city_objs = storage.all("City").values()
-    for obj in city_objs:
-        city_list.append(obj.to_dict())
-    return jsonify(city_list)
-
-
-@app_views.route('/states/<state_id>/cities', methods=["GET"])
+@app_views.route('/states/<state_id>/cities', methods=["GET"],
+                 strict_slashes=False)
 def cities_in_state(state_id):
     """Retrieves all City objects in a particular state"""
-    city_objs = storage.get("City", state_id)
-    if city_objs is None:
+    state_objs = storage.get(State, state_id)
+    if state_objs is None:
         abort(404)
 
-    return jsonify(city_objs.to_dict())
+    list_cities = []
+    cities_dict = storage.all(City).values()
+    for each_city in cities_dict:
+        if state_id == city.state_id:
+            list_cities.append(each_city.to_dict())
+    return jsonify(list_cities)
 
 
-@app_views.route('/cities/<city_id>', methods=["GET"])
+@app_views.route('/cities/<city_id>', methods=["GET"],
+                 strict_slashes=False)
 def cities_retrieval(city_id):
     """Retrieves a City object"""
     city_obj = storage.get(City, city_id)
     if city_obj is None:
         abort(404)
-
     return jsonify(city_obj.to_dict())
 
 
-@app_views.route('/cities/<city_id>', methods=["GET"])
+@app_views.route('/cities/<city_id>', methods=["GET"],
+                 strict_slashes=False)
 def cities_delete(city_id):
     """Deletes a City object"""
     city_obj = storage.get(City, city_id)
     if city_obj is None:
         abort(404)
-
     storage.delete(city_obj)
     storage.save()
     return jsonify({}), 200
 
 
-@app_views.route('/states/<state_id>/cities', methods=["POST"])
+@app_views.route('/states/<state_id>/cities', methods=["POST"],
+                 strict_slashes=False)
 def new_city(state_id):
     """Creates a new City object"""
-    if storage.get(State, state_id) is None:
+    state_obj = storage.get(State, state_id)
+    if state_obj is None:
         abort(404)
 
     city_data = request.get_json()
@@ -63,17 +61,27 @@ def new_city(state_id):
     if city_data.get("name") is None:
         abort(400, "Missing name")
     # request.get_json transforms the HTTP body request to a dict
-    return jsonify(City.post(city_data, state_id)), 201
+    new_city = City(name=city_data["name"], state_id=state_id)
+    new_city.save()
+    return jsonify(new_city.to_dict()), 201
 
 
-@app_views.route('/cities/<city_id>', methods=["PUT"])
+@app_views.route('/cities/<city_id>', methods=["PUT"],
+                 strict_slashes=False)
 def update_city(city_id):
     """Updates a City object"""
-    if storage.get(City, city_id) is None:
+    city_obj = storage.get(City, city_id)
+    if city_obj is None:
         abort(404)
 
+    new_dict = {}
+    ignore_list = ["id", "created_at", "updated_at"]
     city_data = request.get_json()
     if city_data is None:
         abort(400, "Not a JSON")
-
-    return jsonify(City.put(city_data, city_id)), 200
+    for key, value in city_data.items():
+        if key not in ignore_list:
+            new_dict[key] = value
+    city_obj.update(new_dict)
+    city_obj.save()
+    return jsonify(city.to_dict()), 200
