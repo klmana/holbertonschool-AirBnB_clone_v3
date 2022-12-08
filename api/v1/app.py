63d4f3ed67b22adeba72aaad6e46ed264792c0e2
Gotmished <5002@holbertonstudents.com>
2022-12-08 12:11:11 +0000

    Editing to pass checker

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,8 +7,9 @@ from os import getenv
 from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins":["0.0.0.0"]}})
+
 app.register_blueprint(app_views)
-CORS(app, origins='0.0.0.0')
 
 
 @app.teardown_appcontext
